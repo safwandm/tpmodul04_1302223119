5fb7bff674dabbea1f10866ecb7a8a78956d9f29
safwandm <safwan.damanik@gmail.com>
2024-03-17 23:57:30 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_1302223119/obj/Debug/net8.0/tpmodul5_1302223119.AssemblyInfo.cs
+++ tpmodul5_1302223119/obj/Debug/net8.0/tpmodul5_1302223119.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302223119")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18acefc3ce4efe8c2785f632af83dcd1b355ccc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302223119")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302223119")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
